f530a3e78ad68dc208dc9805691f82439a3931a9
amr <amrreda547@gmail.com>
2020-02-17 11:19:44 +0200

    add typescript examples

---
+++ index.vuln.ts
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+const port = 3000
+import { Response, Request } from "express";
+
+import needle from "needle";
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const Vulnerability1 = (req: Request, res: Response) => {
+    var url = req.query['url'];
+    needle.get(url, { timeout: 3000 }, function(error, response1) {
+        if (!error && response1.statusCode == 200) {
+            res.write(response1.body);
+            res.end();
+        } else {
+            res.end();
+            console.log('error')
+        }
+    });
+}
+    
+const Vulnerability2 = (req: Request, res: Response) => {
+    needle('put', req.query['url'], { state: 'god' }, { json: true })
+    .then(function(response) {
+        // doSomethingWith(response)
+        res.end();
+    })
+    .catch(function(err) {
+        console.log('Call the locksmith!')
+        res.end();
+    })
+}
+
+app.get('/vuln1', Vulnerability1);
+app.get('/vuln2', Vulnerability2);
